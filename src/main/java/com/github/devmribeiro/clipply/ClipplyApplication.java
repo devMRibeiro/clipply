d73e785a494c40adaf01cbb0b8a431253cac14ea
devMRibeiro <michael.douglas2020@hotmail.com>
2026-03-12 11:39:17 -0300

    First commit

---
+++ src/main/java/com/github/devmribeiro/clipply/ClipplyApplication.java
@@ -0,0 +1,13 @@
+package com.github.devmribeiro.clipply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/devmribeiro/clipply/ClipplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.devmribeiro.clipply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
