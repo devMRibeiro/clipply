88cf30841795d166e16eae92f0a732fd5242ad1a
devMRibeiro <michael.douglas2020@hotmail.com>
2026-03-17 18:57:27 -0300

    chore: adjust formatting and EOF newline

--- src/main/java/com/github/devmribeiro/clipply/ClipplyApplication.java
+++ src/main/java/com/github/devmribeiro/clipply/ClipplyApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClipplyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ClipplyApplication.class, args);
 	}
-
 }
