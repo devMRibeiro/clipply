8b9df73eb5e437285b7c3f866c94a7ba60c74c0e
devMRibeiro <michael.douglas2020@hotmail.com>
2026-03-19 07:41:13 -0300

    feat: add bootstrap data to create default support user

---
+++ src/main/java/com/github/devmribeiro/clipply/application/util/BootstrapData.java
@@ -0,0 +1,50 @@
+package com.github.devmribeiro.clipply.application.util;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.github.devmribeiro.clipply.application.model.User;
+import com.github.devmribeiro.clipply.application.repository.UserRepository;
+import com.github.devmribeiro.clipply.application.type.UserRole;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+	private final UserRepository userRepository;
+	private final PasswordEncoder encoder;
+	
+	@Value("${clipply.support-email}")
+	private String emailSupport;
+
+	@Value("${clipply.support-password}")
+	private String passwordSupport;
+
+	@Value("${clipply.support-phone}")
+	private String phoneSupport;
+	
+	public BootstrapData(
+			UserRepository userRepository,
+			PasswordEncoder encoder) {
+		this.userRepository = userRepository;
+		this.encoder = encoder;
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		if (userRepository.existsByEmail(emailSupport))
+			return;
+
+		User user = new User();
+		user.setEmail(emailSupport);
+		user.setPassword(encoder.encode(passwordSupport));
+		user.setPhone(phoneSupport);
+		user.setRole(UserRole.SUPPORT);
+		user.setPasswordChagedAt(LocalDateTime.now());
+		userRepository.save(user);
+	}
+}
